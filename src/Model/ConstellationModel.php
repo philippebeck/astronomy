e2de48f0e1677e934eab72eb7d7fbdb305fe7c2e
Philippe Beck <philippe@philippebeck.net>
2020-06-23 10:22:55 +0200

    Create all other Models

---
+++ src/Model/ConstellationModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\MainModel;
+
+/**
+ * Class ConstellationModel
+ * @package App\Model
+ */
+class ConstellationModel extends MainModel {}
---
+++ src/Model/MapModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\MainModel;
+
+/**
+ * Class MapModel
+ * @package App\Model
+ */
+class MapModel extends MainModel {}
